d1065ee060073d7603bb123e094d12954b3c0c63
Deepanshu pal <deepanshupal3510e@gmail.com>
2025-09-22 23:50:15 +0530

    no changes

--- socialmedia/Frontend/src/services/endpoint.js
+++ socialmedia/Frontend/src/services/endpoint.js
@@ -1,7 +1,11 @@
 export const base_url = import.meta.env.VITE_BACKEND_URL
-const api={
- user:{
-    getUserDetails:base_url+"/api/user/get-user-details",
- }
+
+const api = {
+   user: {
+      getUserDetails: base_url + "/api/user/get-user-details",
+   },
+   post : {
+      
+   }
 }
 export default api
